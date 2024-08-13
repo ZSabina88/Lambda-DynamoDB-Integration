5dfc4b4564523749c9ffc192cf9c3cebe70122dd
Sabina Zeynalova <117943057+ZSabina88@users.noreply.github.com>
2024-08-13 23:38:39 +0400

    fix

--- task05/app/lambdas/api_handler/index.js
+++ task05/app/lambdas/api_handler/index.js
@@ -4,24 +4,31 @@ const AWS = require("aws-sdk");
 const docClient = new AWS.DynamoDB.DocumentClient();
 const tableName = "cmtr-31f1a922-Events-test";
 
+
 exports.handler = async (event) => {
-  const params = {
-    TableName: tableName,
-    Item: {
-      id: uuidv4(),
-      principalId: event.principalId,
-      createdAt: new Date().toISOString(),
-      body: event.content,
-    },
-  };
-  const response = {
-    statusCode: 201,
-    event: params.Item,
-  };
-  try {
-    await docClient.put(params).promise();
-    return response;
-  } catch (err) {
-    return JSON.stringify(err, null, 2);
-  }
+    const { principalId, content } = JSON.parse(event.body);
+    const id = uuid.v4();
+    const createdAt = new Date().toISOString();
+
+    const item = {
+        id,
+        principalId,
+        createdAt,
+        body: content
+    };
+
+    const params = {
+        TableName: tableName,
+        Item: item
+    };
+    try {
+        await docClient.put(params).promise();
+        return {
+            statusCode: 201,
+            event: params.Item,
+        };;
+    } catch (err) {
+        return JSON.stringify(err, null, 2);
+    }
+
 };
