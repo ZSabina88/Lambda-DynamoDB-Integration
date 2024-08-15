28064da510a2ed2390cf25a76f306f50b12a4813
Sabina Zeynalova <117943057+ZSabina88@users.noreply.github.com>
2024-08-15 14:17:41 +0400

    fix

--- task05/app/lambdas/api_handler/index.js
+++ task05/app/lambdas/api_handler/index.js
@@ -5,52 +5,23 @@ const dynamo = new AWS.DynamoDB.DocumentClient();
 const tableName = 'cmtr-31f1a922-Events-test';
 
 exports.handler = async (event) => {
-   
-    let parsedBody;
-    try {
-        parsedBody = JSON.parse(event.body);
-    } catch (err) {
-        return {
-            statusCode: 400,
-            body: JSON.stringify({ error: "Invalid JSON in request body" }),
-        };
-    }
-
-    const { principalId, content } = parsedBody;
-
-    if (typeof principalId !== 'number' || !content || typeof content !== 'object') {
-        return {
-            statusCode: 400,
-            body: JSON.stringify({ error: "Missing or invalid 'principalId' or 'content'" }),
-        };
-    }
-
-    const id = uuid.v4();
-    const createdAt = new Date().toISOString();
-
-    const item = {
-        id,
-        principalId,
-        createdAt,
-        body: content
-    };
-
     const params = {
         TableName: tableName,
-        Item: item
+        Item: {
+            id: uuid.v4(),
+            principalId: event.principalId,
+            createdAt: new Date().toISOString(),
+            body: event.content,
+        },
     };
-
+    const response = {
+        statusCode: 201,
+        event: params.Item,
+    };
     try {
         await dynamo.put(params).promise();
-    } catch (error) {
-        return {
-            statusCode: 500,
-            body: JSON.stringify({ error: "Failed to save event to DynamoDB" }),
-        };
+        return response;
+    } catch (err) {
+        return JSON.stringify(err, null, 2);
     }
-
-    return {
-        statusCode: 201,
-        body: JSON.stringify({ event: item }),
-    };
 };
